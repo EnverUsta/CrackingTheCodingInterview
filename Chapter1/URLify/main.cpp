0ea3a94ac8d571d2879a785f586bb26744f84e90
Enver <enver.usta.3@gmail.com>
2021-04-29 20:18:36 +0300

    URLify added

---
+++ Chapter1/URLify/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Q. Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient
+    space at the end to hold the additional characters, and that you are given the "true" length of the string.
+
+    examples:
+        "Mr John Smith    " => "Mr%20John%20Smith"
+        "Mr%20J" => O(n^2)  it is not efficient === Brute Force Solution
+        string temp = "Mr%20John%20Smith"
+*/
+
+
+// time-complexity : O()
+// space-complexity : O()
+void URLify(string& input)
+{
+    string temp;
+
+    int count = 0;
+    for(auto ch : input)
+    {
+        if(ch == ' ' &&  count != input.size()-1 && input[count+1] != ' ')
+        {
+            temp.push_back('%');
+            temp.push_back('2');
+            temp.push_back('0');
+
+        }
+        else if(ch == ' ')
+            continue;
+        else
+            temp.push_back(ch);
+
+        count++;
+    }
+
+    input = temp;
+}
+
+int main()
+{
+    cout << "Hello" << '\n';
+    string input = "Mr John Smith    ";
+    URLify(input);
+    cout << input << '\n';
+}
